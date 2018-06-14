86f8af41f93fece5115698a5a386016f36f64793
Winona Oliveros Diez <35917727+wod31@users.noreply.github.com>
2018-06-14 10:41:33 +0200

    Update x2_get_counts.sh

--- x2_get_counts.sh
+++ x2_get_counts.sh
@@ -9,6 +9,10 @@
 #module load R/3.2.3
 #Rscript x2_rnaseq_get-counts.R
 
+#$1 = directory with sample files (fastq)
+#$2 = GTF file ; input the whole path
+
+
 mkdir htscount/
 module load parallel/20151222
 module load HTSeq/0.9.1-foss-2016b-Python-2.7.12
@@ -17,8 +21,8 @@ function counts() {
     base=`basename "$1" | sed 's/.fastq.gz//g'`
     echo "processing "$base
     ### counts from bam sorted by position and not strand-specific assay
-    htseq-count -f bam -r pos -s reverse star/$base\Aligned.sortedByCoord.out.bam gtf/Schizosaccharomyces_pombe.ASM294v2.35.chr.gtf > htscount/$base\_htseq.csv
+    htseq-count -f bam -r pos -s reverse star/$base\Aligned.sortedByCoord.out.bam $2 > htscount/$base\_htseq.csv
 }
 export -f counts
 
-ls *.fastq.gz | parallel --progress  -k counts {}
+ls $1*.fastq.gz | parallel --progress  -k counts {}
